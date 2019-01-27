6f9b22d70d0b23bfc226cc2daa47fb51b5620c67
Jungbom Pak <jungbompak@Jungboms-MacBook-Pro.local>
2019-01-27 09:57:10 -0800

    Initial Commit

--- entry_point.py
+++ entry_point.py
@@ -2,4 +2,5 @@
 import scrapy
 from scrapy.cmdline import execute
 
-ret = execute(['scrapy', 'crawl', 'coursera', '-o', 'machine learning.json', '-a', 'query=Global Human Rights Teach-Out', '-a', 'language=English'])
+ret = execute(['scrapy', 'crawl', 'coursera', '-o', 'machine learning.json', '-a', 'query=machine learning', '-a', 'language=English'])
+# ret = execute(['scrapy', 'crawl', 'coursera', '-o', 'machine learning.json', '-a', 'query=machine learning'])
